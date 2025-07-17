bf0b23b8a93a84b3d300abbfc8f14b687d76c3ea
achak <136048425+Achak0120@users.noreply.github.com>
2025-07-16 22:21:36 -0500

    clean_data with CSV

--- src/REV_src/analyze_data/analyze_methods.py
+++ src/REV_src/analyze_data/analyze_methods.py
@@ -10,7 +10,14 @@ from pathlib import Path
 
 def clean_data(log_path: str) -> pd.DataFrame:
     df = pd.read_csv(log_path)
+    df.to_csv("full_output.csv", index=False)  # Don't assign it back to df
+    
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    print(df)
 
 
-csv_path = Path(__file__).resolve().parents[2] / 'test_logs' / 'ILCH_Q70_rio_2025-03-29_11-42-10_CSV.csv'
-clean_data('')
+csv_path = Path(r"C:\Users\Aishik C\Desktop\ARCS\ARCS\src\test_logs\ILCH_Q70_rio_2025-03-29_11-42-10_CSV.csv")
+clean_data(csv_path)
